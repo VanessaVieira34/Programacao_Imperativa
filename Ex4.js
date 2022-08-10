0b1e50224321b309770038a754991c66e954fe0e
Vanessa Vieira <105254938+VanessaVieira34@users.noreply.github.com>
2022-08-09 22:08:46 -0300

    Add files via upload

---
+++ Ex4.js
@@ -0,0 +1,9 @@
+const nome = "Jose"; 
+const sobrenome = "Da silva"; 
+let idade = 27; 
+let peso = 83.5;
+let altura = 1.70;
+let plano = true; 
+let imc = peso / Math.pow(altura,2); 
+
+console.log(nome + " " + sobrenome + " Tem " + idade + " anos e seu indice de massa corporal é de " + imc);
