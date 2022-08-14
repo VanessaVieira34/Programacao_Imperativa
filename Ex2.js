e501feca741b7adb0d0f4e95db5e254c0086e713
Vanessa Vieira <105254938+VanessaVieira34@users.noreply.github.com>
2022-08-14 18:33:52 -0300

    Add files via upload

---
+++ Ex2.js
@@ -0,0 +1,7 @@
+var produto = "Pizza de Calabresa"
+var id = 03
+var temNoEstoque = false
+
+console.log("Produto: " + produto);
+console.log("ID: " +id)
+console.log("Disponivél: " + temNoEstoque);
---
+++ Ex3.js
@@ -0,0 +1,16 @@
+console.log(5 + 3);
+console.log(6 * 8);
+
+var num1 = 8;
+var num2 = 6;
+
+console.log(num1 + num2);
+
+var numResult = "O Resultado é: " + (num1 + num2);
+console.log(numResult);
+
+var numResult = "1" + 1;
+console.log(numResult);
+
+var numResult = "ola" + 23;
+console.log(numResult);
